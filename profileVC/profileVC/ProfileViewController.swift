eb172e6d6cbb38105d08691a111df5d411ecee8e
Nayeon Chae <cny1230@naver.com>
2024-04-23 12:00:30 +0900

    upload the empty file first

---
+++ profileVC/profileVC/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  profileVC
+//
+//  Created by 채나연 on 4/23/24.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
